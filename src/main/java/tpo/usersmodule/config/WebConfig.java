bd10b08ad22dd8fea25c5145447b85add4ad7c20
Patricio Garcia <144806948+patofgarcia1@users.noreply.github.com>
2024-10-12 21:13:17 -0300

    Add files via upload

---
+++ src/main/java/tpo/usersmodule/config/WebConfig.java
@@ -0,0 +1,18 @@
+package tpo.usersmodule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas las rutas
+                .allowedOrigins("https://main.d220bqspbr415z.amplifyapp.com") // Cambia por tu dominio
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Si necesitas permitir credenciales
+    }
+}
